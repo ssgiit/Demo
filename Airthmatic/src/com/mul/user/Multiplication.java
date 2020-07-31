f357d13f49057d90133ed670012bf1770fce2842
Shree <sanket9darekar9@gmail.com>
2020-07-31 17:40:35 +0530

    they commit

---
+++ Airthmatic/src/com/mul/user/Multiplication.java
@@ -0,0 +1,13 @@
+package com.mul.user;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+
+		int a=2;
+		int b=4;
+		System.out.println("Multiplication is "+(a*b));
+		
+	}
+
+}
