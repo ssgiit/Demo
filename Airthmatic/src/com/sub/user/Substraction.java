1bf85f709fde4b1a8d7ea53ff8569361073df7ba
Shree <sanket9darekar9@gmail.com>
2020-07-31 17:23:14 +0530

    committed by sanket

---
+++ Airthmatic/src/com/sub/user/Substraction.java
@@ -0,0 +1,15 @@
+package com.sub.user;
+
+import com.add.user.Addition;
+
+public class Substraction{
+
+	public static void main(String[] args) 
+	{
+		int a=20;
+		int b=3;
+
+		System.out.println("Substraction is "+(a-b));
+	}
+
+}
